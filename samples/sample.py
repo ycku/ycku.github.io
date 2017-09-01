2f4e0b51e6ab13efd0f30a493dde8bf2d06ca1bc
Yung-Chung Ku <ycku@csie.org>
2017-09-01 16:03:38 +0800

    Create sample.py

---
+++ samples/sample.py
@@ -0,0 +1,29 @@
+# 執行時自行註解掉不需要的段落
+
+# 自動型別
+var = 'Hello World'  # string
+print(var)
+var = 100            # int
+print(var+10)
+print('-----')
+
+# 沒有 overflow
+var = 17**3000  # 17的3000次方
+print(var)
+print('-----')
+
+# swap
+a=1
+b=2
+c=3
+print(a,b,c)
+c,a,b=b,c,a
+print(a,b,c)
+print('-----')
+
+# string index
+var1 = 'Hello World'
+var2 = "Python Programming"
+print(var1[0]) # H, 從0開始
+print(var2[1:5]) # "ytho", 1到小於5
+print('-----')
