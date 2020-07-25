0214843359560638e86ca03109bf5114a895a89b
Yung-Chung Ku <ycku@csie.org>
2020-07-25 15:30:48 +0800

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -17,7 +17,7 @@ function refresh_me() {
 }
 
 function switch_wrapper() {
-	var max_post_number=5;
+	var max_post_number=6;
 	var status=document.getElementById('wrapper').style.visibility;
 	if (status=='hidden' || status==false) {
 		var n=Math.floor(Math.random()*(max_post_number+1));
